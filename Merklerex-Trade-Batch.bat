017157f463231057e7c4957c022e9271cde10d8d
Mohamed Galal <MohamedAhmedGalal74_sd@eng.bsu.edu.eg>
2023-10-21 20:48:29 +0300

    Editted batch file

--- Merklerex-Trade-Batch.bat
+++ Merklerex-Trade-Batch.bat
@@ -1,7 +1,3 @@
 g++ --std=c++11 "Source Files\colors.cpp" "Source Files\CSVReader.cpp" "Source Files\main.cpp" "Source Files\MerkelMain.cpp" "Source Files\orderBook.cpp" "Source Files\orderBookEntry.cpp" "Source Files\wallet.cpp" -o Merklex-Trade.exe
-<<<<<<< HEAD
 cls
-=======
-
->>>>>>> e6fd21f014c5fdb611f2230c193ca9241b372959
 Merklex-Trade.exe
